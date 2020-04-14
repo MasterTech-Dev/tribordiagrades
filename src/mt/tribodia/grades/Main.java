16b23423bb9b1c674283c7ba305938f58c1ced58
MasterTech-Dev <mastertech.officiel@gmail.com>
2020-04-14 19:00:04 +0200

    Ajout de données

---
+++ src/mt/tribodia/grades/Main.java
@@ -0,0 +1,16 @@
+package mt.tribodia.grades;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	/// WHEN THE PLUGIN STARTS
+	@Override
+	public void onEnable() {
+		super.onEnable(); // Optimization
+	}
+	/// WHEN THE PLUGIN SHUTDOWN
+	@Override
+	public void onDisable() {
+		super.onDisable(); // Optimization
+	}
+}
